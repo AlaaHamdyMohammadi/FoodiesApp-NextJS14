4de7b090de9d16cc33f90934b96b3452fb3df67b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-07 22:17:59 +0200

    Styling

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,24 +1,25 @@
 import Link from "next/link"
 import logoImg from '@/assets/logo.png';
+import classes from './MainHeader.module.css';
 function MainHeader() {
     return (
-        <header>
-            <Link href="/">
-                <img src={logoImg.src} alt="logo"/>
-                NextLevel Food
-            </Link>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Foodies Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <img src={logoImg.src} alt="logo" />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
 }
 
 export default MainHeader
