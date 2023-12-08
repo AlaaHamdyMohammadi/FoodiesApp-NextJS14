4fd565f248d5aeea3ecdbc7bc3b7816ec2335f03
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-08 18:33:16 +0200

    Update Links

--- components/mainHeaders/MainHeader.js
+++ components/mainHeaders/MainHeader.js
@@ -4,8 +4,10 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./MainHeader.module.css";
 import MainHeaderBackground from "./MainHeaderBackground";
+import NavLink from "./NavLink";
 
 function MainHeader() {
+  
   return (
     <>
       <MainHeaderBackground />
@@ -18,10 +20,10 @@ function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
