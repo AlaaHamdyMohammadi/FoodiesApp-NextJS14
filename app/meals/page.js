17d61222a12230c13c005ddc1f0092e280709447
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-08 23:17:15 +0200

    Fetching Data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/MealsGrid';
+import { getMeals } from '@/lib/meals';
 
-function MealsPage() {
+async function MealsPage() {
+  const meals = await getMeals();
     return (
       <>
       <header className={classes.header}>
@@ -13,7 +15,7 @@ function MealsPage() {
         </p>  
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
       </>
     );
